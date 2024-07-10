e5c145c6007b2485e1005e2daaa4fe5a29b75094
Sujit <shegde@walterpmoore.com>
2024-07-10 20:48:51 +0530

    Added usage tracking

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
+
+
+class Usage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event = db.Column(db.String(80), nullable=False)
+    user = db.Column(db.String(80), nullable=False)
+    timestamp = db.Column(db.String(80), nullable=False)
+
+# Route to handle incoming data
+@app.route('/track', methods=['POST'])
+def track_usage():
+    try:
+        data = request.json
+        event = data.get('e')
+        timestamp = data.get('timestamp')
+        user=data.get('user')
+        print(f"Received data: event: {event}, timestamp: {timestamp},user:{user}")
+        
+        new_usage = Usage(event=event, user=user, timestamp=timestamp)
+        db.session.add(new_usage)
+        db.session.commit()
+
+        return jsonify({"status": "success"}), 200
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        dblist=Usage.query.all()
+        for item in dblist:
+            print(f"ID: {item.id}, Event: {item.event}, User: {item.user}, Timestamp: {item.timestamp}")
+            
+    app.run(debug=True)
+    
